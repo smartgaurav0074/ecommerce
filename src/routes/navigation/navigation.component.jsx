f4b796721a1c586c3c5d1c3f3a5225e98699de33
Gaurav Kumar <smartgaurav0074@gmail.com>
2022-12-02 07:28:21 +0530

    created authentication page component alongside sign in form

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,27 +1,26 @@
-import { Fragment } from "react"
-import { Outlet, Link } from "react-router-dom"
-import { ReactComponent as CrownLogo} from '../../assets/crown.svg'
+import { Fragment } from 'react'
+import { Outlet, Link } from 'react-router-dom'
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
 import './navigation.style.scss'
 const Navigation = () => {
     return (
-      <Fragment>
-        <div className="navigation" >
-          <Link className='logo-container' to='/'>
-            <CrownLogo />
-          </Link>
-          <div className='nav-links-container'>
-            <Link className='nav-link' to='/shop'>
-              SHOP
-            </Link>
-            <Link className='nav-link' to='/sign-in'>
-              SIGNIN
-            </Link>
-          </div>
-            
-        </div>
-        <Outlet />
-      </Fragment>
+        <Fragment>
+            <div className="navigation">
+                <Link className="logo-container" to="/">
+                    <CrownLogo />
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to="/shop">
+                        SHOP
+                    </Link>
+                    <Link className="nav-link" to="/auth">
+                        SIGNIN
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
     )
-  }
+}
 
-  export default Navigation
+export default Navigation
